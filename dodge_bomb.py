3dd75b8a64a8d9992ca5f19c902935f69580dfb4
c0a2307566 <c0a2307566@edu.teu.ac.jp>
2024-10-01 17:32:20 +0900

    こうかとんの向き

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -31,7 +31,7 @@ def check_bound(obj_rct:pg.Rect) -> tuple[bool, bool]:
 
 def game_over(screen:pg.display):
     """
-    引数：スクリーン
+    引数：最初に作ったディスプレイ
     戻り値：なし
     画面をブラックアウトし, 泣いているこうかとんと文字列を時間分表示
     """
@@ -55,6 +55,17 @@ def game_over(screen:pg.display):
     time.sleep(5)
 
 
+#def kasoku()
+
+
+def return_dic(mv:tuple, dic:dict) -> tuple:
+    """
+    引数：移動量と画像辞書
+    戻り値：画像
+    """
+    mv = tuple(mv)
+    return dic[mv]
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -62,11 +73,34 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    kks_img = []
+    mvs_lis = [(-5, -5), (-5, 0), (-5, 5), (0, 5),
+               (5, 5), (5, 0), (5, -5), (0, -5), ]
+    for k in range(8):
+        if k==3:
+            kks_img.append(pg.transform.flip((pg.transform.rotozoom(kk_img, 45, 1.0)), True, False))
+            continue
+        if k==4:
+           kks_img.append(pg.transform.flip(kks_img[2], True, False)) 
+           continue
+        if k==5:
+            kks_img.append(pg.transform.flip(kks_img[1], True, False))
+            continue
+        if k==6:
+            kks_img.append(pg.transform.flip(kks_img[0], True, False))
+            continue
+        if k==7:
+            kks_img.append(pg.transform.flip(kks_img[3], False, True))
+            continue
+        kks_img.append(pg.transform.rotozoom(kk_img, 45*k-45, 1.0))
+    kks_img = dict(zip(mvs_lis, kks_img))
     bb_img = pg.Surface((20, 20))
     bb_img.set_colorkey((0, 0, 0))
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
     bb_rct = bb_img.get_rect()
-    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT) 
+    bb_imgs = []
     #for r in range(1, 11):
         #bb_img = pg.Surface((20*r, 20*r))
         #pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
@@ -92,6 +126,8 @@ def main():
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+        if sum_mv != [0, 0]:
+            kk_img = return_dic(sum_mv, kks_img)
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx, vy)
         yoko, tate = check_bound(bb_rct)
