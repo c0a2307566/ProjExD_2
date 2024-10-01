a593ba9e5cc978d5079512f15e467a7fbf970299
c0a2307566 <c0a2307566@edu.teu.ac.jp>
2024-10-01 16:23:22 +0900

    ゲームオーバー作成

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,7 +2,7 @@ import os
 import sys
 import pygame as pg
 import random
-
+import time
 
 WIDTH, HEIGHT = 1100, 650
 DELTA = {
@@ -11,6 +11,7 @@ DELTA = {
         pg.K_LEFT:(-5, 0),
         pg.K_RIGHT:(5, 0),
         }
+#accs = [a for a in range(1, 11)]
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -28,6 +29,32 @@ def check_bound(obj_rct:pg.Rect) -> tuple[bool, bool]:
     return yoko, tate
 
 
+def game_over(screen:pg.display):
+    """
+    引数：スクリーン
+    戻り値：なし
+    画面をブラックアウトし, 泣いているこうかとんと文字列を時間分表示
+    """
+    go_img = pg.Surface((WIDTH, HEIGHT))
+    pg.draw.rect(go_img, (0, 0, 0), pg.Rect(0, 0, WIDTH, HEIGHT))
+    go_img.set_alpha(200)
+    screen.blit(go_img, [0, 0])
+    fonto = pg.font.Font(None, 80)
+    txt = fonto.render("Game Over", True, (255, 255, 255))
+    txt_rect = txt.get_rect()
+    txt_rect.center = (WIDTH//2, HEIGHT//2)
+    screen.blit(txt, txt_rect)
+    gokk_img = pg.transform.rotozoom(pg.image.load("fig/8.png"), 0, 0.9)
+    gokk1_rect = gokk_img.get_rect()
+    gokk1_rect.center = (300, HEIGHT//2)
+    gokk2_rect = gokk_img.get_rect()
+    gokk2_rect.center = (800, HEIGHT//2)
+    screen.blit(gokk_img, gokk1_rect)
+    screen.blit(gokk_img, gokk2_rect)
+    pg.display.update()
+    time.sleep(5)
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -40,6 +67,9 @@ def main():
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
     bb_rct = bb_img.get_rect()
     bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    #for r in range(1, 11):
+        #bb_img = pg.Surface((20*r, 20*r))
+        #pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
     vx = 5
     vy = 5
     clock = pg.time.Clock()
@@ -50,6 +80,7 @@ def main():
                 return
         screen.blit(bg_img, [0, 0]) 
         if kk_rct.colliderect(bb_rct):
+            game_over(screen)
             return
 
         key_lst = pg.key.get_pressed()
